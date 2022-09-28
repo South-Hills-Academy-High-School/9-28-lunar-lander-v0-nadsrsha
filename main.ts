ef783c5c097db5664e9e646029768f7c5f6462f5
nadsrsha <113388205+nadsrsha@users.noreply.github.com>
2022-09-28 12:22:17 -0700

    scaling fixed

--- main.ts
+++ main.ts
@@ -1,1 +1,45 @@
-
+namespace SpriteKind {
+    export const map = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.ay = -25
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundCenter, function (sprite, location) {
+    mySprite.setVelocity(0, -1)
+})
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    mySprite.ay = 20
+})
+let mySprite: Sprite = null
+tiles.setCurrentTilemap(tilemap`level1`)
+effects.starField.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . . . . e c 7 . . . . . . 
+    . . . . e e e c 7 7 e e . . . . 
+    . . c e e e e c 7 e 2 2 e e . . 
+    . c e e e e e c 6 e e 2 2 2 e . 
+    . c e e e 2 e c c 2 4 5 4 2 e . 
+    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+    . . . 2 2 e e 4 4 4 2 e e . . . 
+    . . . . . 2 2 e e e e . . . . . 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+scaling.scaleByPercent(mySprite, -25, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+mySprite.ay = 20
+let myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
+let mySprite2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
+game.onUpdate(function () {
+    mySprite2.destroy()
+    myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
+    minimap.includeSprite(myMinimap, mySprite, MinimapSpriteScale.MinimapScale)
+    mySprite2 = sprites.create(minimap.getImage(minimap.minimap()), SpriteKind.map)
+    mySprite2.setPosition(mySprite.left, mySprite.top)
+})
